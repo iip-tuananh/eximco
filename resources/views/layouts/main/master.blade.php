75de8d9559bb4a23af80318a9f338b512ccd9603
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2023-02-02 08:13:10 +0700

    fixed

--- resources/views/layouts/main/master.blade.php
+++ resources/views/layouts/main/master.blade.php
@@ -65,7 +65,6 @@
     <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet"
         id="aHR0cHM6Ly9jcm0uYml6Zmx5LnZuL2JpemZseS1jbGllbnQvc3R5bGUuY3NzP3Zlcj0z" type="text/css">
     <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/aml-widget.min.css') }}">
-    {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&amp;display=swap"> --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
